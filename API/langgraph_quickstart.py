18034f29635090fc6e9c0a4eb4463a9c382af710
AidenTag <87206143+AidenTag@users.noreply.github.com>
2024-11-23 22:13:29 -0500

    Optimized and commented langgraph_quickstart, poised to reorganize agentic code structure upcoming

--- API/langgraph_quickstart.py
+++ API/langgraph_quickstart.py
@@ -18,97 +18,47 @@ from langchain_core.tools import tool
 from langgraph.prebuilt import ToolNode
 
 load_dotenv()
-
-calendar_service = GoogleCalendarService()
-service = calendar_service.service
-
 GOOGLE_API_KEY = os.getenv("GEMINI_API_KEY")
 
+service = GoogleCalendarService()
+
+# Define classes for use in tools and graph
 
 class State(TypedDict):
-   # Messages have the type "list". The `add_messages` function
-   # in the annotation defines how this state key should be updated
-   # (in this case, it appends messages to the list, rather than overwriting them)
    messages: Annotated[list, add_messages]
-
-class EventTime(BaseModel):
-   """Structure for the time properties of the event body"""
-
-   dateTime: str = Field(..., description="DateTime in ISO 8601 format")
-   timeZone: str = Field(..., description="Time zone for the event")
-
 
 class EventBody(BaseModel):
    """Event body for Google Calendar event"""
 
-   summary: str = Field(description="Title of the event")
-   start: EventTime = Field(..., description="Start time of the event")
-   end: EventTime = Field(..., description="End time of the event")
+   summary: str = Field(description="Title of the event", example="Meeting with John")
+   startTime: str = Field(..., description="DateTime in ISO 8601 format, starting time of the event", example="2024-10-18T10:00:00")
+   endTime: str = Field(..., description="DateTime in ISO 8601 format, endgin time of the event", example="2024-10-18T11:00:00")
+   timeZone: str = Field(..., description="Time zone for the event", example="America/New_York")
 
-@tool(args_schema=EventBody, parse_docstring=True)
+@tool(args_schema=EventBody)
 def add_event(
    summary: str,
-   start: EventTime,
-   end: EventTime):
-   """Method to insert event into Google Calendar using API.Method to insert event into Google Calendar using API.
-    The args must follow this exact structure:
-    {
-        "summary": "Meeting with John",
-        "start": {
-            "dateTime": "2024-03-20T14:00:00",
-            "timeZone": "America/New_York"
-        },
-        "end": {
-            "dateTime": "2024-03-20T15:00:00",
-            "timeZone": "America/New_York"
-        }
-    }"""
+   startTime: str,
+   endTime: str,
+   timeZone: str):
+   """Method to insert event into Google Calendar using API."""
    try:
-      event_body = EventBody(summary=summary, start=start, end=end).model_dump()
+      # Construct event body from args (still unsure about this system, was designed for best AI understanding of args)
+      event_body = {
+         "summary": summary,
+         "start": {
+            "dateTime": startTime,
+            "timeZone": timeZone
+         },
+         "end": {
+            "dateTime": endTime,
+            "timeZone": timeZone
+         },
+      }
       event = service.events().insert(calendarId='primary', body=event_body).execute()  # Insert event
-      print('Event created: ', event_body)
-      webbrowser.open(event.get('htmlLink'))  # Open event in Google Calendar UI
+      #webbrowser.open(event.get('htmlLink')) # Open event in Google Calendar UI
    except Exception as e:
       print(f"""{e}\nUnexpected input format: {event_body}""")
-
-prompt = """
-   You are the Event Initializer agent for a time management AI figure called Indigo. 
-   You will take input detailing an event to insert, and make the appropriate call to `add_event`.
-   The correct format will contain:
-      - "summary": a short title for the event,
-      - "start": the start time in ISO 8601 format,
-      - "end": the end time in ISO 8601 format,
-      - "timeZone": the time zone for the event.
-      An example format is this:
-      {{
-         'summary': 'Meeting with John',
-         'start': {{
-            'dateTime': '2024-10-18T10:00:00',
-            'timeZone': 'America/New_York',
-         }},
-         'end': {{
-            'dateTime': '2024-10-18T11:00:00',
-            'timeZone': 'America/New_York',
-         }},
-      }}
-   Output a message detailing how you found this task.
-   For now, even if appropriate information is missing, fill it in to the best of your abilities.
-   For context, right now the time is {} in {} timezone.
-""" # then make a circular chain based on whether questions are answered.
-
-tools = [add_event]
-tool_node = ToolNode(tools)
-
-llm = ChatGoogleGenerativeAI(
-   model="gemini-1.5-pro", 
-   google_api_key=GOOGLE_API_KEY,
-   temperature=0.5,
-   top_p=0.95,
-   top_k=64,
-   max_output_tokens=8192,
-).bind_tools(tools)
-
-graph_builder = StateGraph(State)
 
 
 def event_init_AI(state: State):
@@ -122,6 +72,28 @@ def event_init_AI(state: State):
    return {"messages": message} # return new 'messages' from invokation of llm on current 'messages' stored in state. Then our add_messages function automically appends
 
 
+prompt = """
+   You are the Event Initializer agent for a time management AI figure called Indigo. 
+   You will take input detailing one or more events to insert, and make the appropriate call to `add_event`. For multiple events, simply make multiple calls to `add_event`.
+   The appropriate argument format for `add_event` is detailed in its description and argument details.
+   Output a content message detailing how you found this task.
+   For context, right now the time is {} in {} timezone.
+""" # Current prompt for LLM, will adapt for circular chain based on whether questions are answered.
+
+
+# Configure tool node and LLM
+tools = [add_event]
+
+tool_node = ToolNode(tools)
+
+llm = ChatGoogleGenerativeAI(
+   model="gemini-1.5-flash", 
+   google_api_key=GOOGLE_API_KEY,
+   temperature=0.2,
+).bind_tools(tools)
+
+# Initialize graph
+graph_builder = StateGraph(State)
 
 graph_builder.add_node("event_init", event_init_AI)
 graph_builder.add_node("process_response", tool_node)
@@ -131,7 +103,7 @@ graph_builder.add_edge("process_response", END)
 
 graph = graph_builder.compile()
 
-output = graph.invoke({"messages": [("user", "Create a meeting with John at 2pm.")]})
-
+# Invoke graph on sample input
+output = graph.invoke({"messages": [("user", "Create me 6 half an hour long meetings Friday afternoon")]})
 
 pprint(output)
